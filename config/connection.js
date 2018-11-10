a8a76eaef66ee8ca57889bd579c0c731fc31ef3e
Renée Grinnell <renee.grinnell@gmail.com>
2018-11-09 22:50:37 -0500

    almost there

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 8080,
     user: 'root',
     password: 'root',
     database: 'bobsburgers_db'
