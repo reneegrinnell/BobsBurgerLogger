a4ccbe88f8339f18cf4e4855786a0cc4823d85eb
Renée Grinnell <renee.grinnell@gmail.com>
2018-11-09 22:06:45 -0500

    finally done with the setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+    burger.all(function(data) {
+        var handlebarsObject = {
+        burgers: data
+    };
+    console.log(handlebarsObject);
+    res.render("index", handlebarsObject);
+    });
+});
+
+router.post("/burgers", function(req, res) {
+    burger.create(
+        ["burger_name"], [req.body.b_name], function() {
+            res.redirect("/burgers");
+        });
+});
+
+router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update(
+    {"devoured": req.body.devoured}, condition, function(data) {
+            res.redirect("/burgers");
+    });
+});
+
+module.exports = router;
