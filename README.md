710be0572a45c908787c64394b85abf5d612469c
Renée Grinnell <renee.grinnell@gmail.com>
2018-11-11 23:14:56 -0500

    updating readme

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Users of the Bob's Burger Logger can create, store, and devour delicious SQL dat
 ## Live Site on Heroku:
 https://nameless-reaches-49971.herokuapp.com/
 
+## Screenshots:
+https://github.com/reneegrinnell/BobsBurgerLogger/blob/master/images/screenshot.PNG
+
 ## Credits
 * Shoutout to [Loren Bouchard](https://en.wikipedia.org/wiki/Loren_Bouchard) for dreaming up [*Bob's Burgers*](https://en.wikipedia.org/wiki/Bob%27s_Burgers).
 * Honorable mention to Paula Forbes at eater.com for this comprehensive list of [every *Bob's Burgers* burger special ever](https://www.eater.com/2011/4/27/6684143/every-burger-special-ever-from-bobs-burgers)!
--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,16 @@ var mysql = require('mysql');
 var connection
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: process.env.PORT || 3306,
-    user: "root",
-    password: "root",
-    database: "bobsburgers_db",
-    use_env_variable: "JAWS_DB_URL"
-  });
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: process.env.PORT || 3306,
+        user: "root",
+        password: "root",
+        database: "bobsburgers_db",
+        use_env_variable: "JAWS_DB_URL"
+    });
 };
 
 connection.connect(function (err) {
