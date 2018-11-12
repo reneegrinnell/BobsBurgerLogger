f954911bdbcc74fc1d586b870a0dabb9fd75187f
Renée Grinnell <renee.grinnell@gmail.com>
2018-11-11 19:01:55 -0500

    styling done, database connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
-    port: 8080,
+    port: 3306,
     user: 'root',
     password: 'root',
     database: 'bobsburgers_db'
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burgername, devoured) VALUES ("Foot Feta-ish Burger", false);
 INSERT INTO burgers (burgername, devoured) VALUES ("Mission A-Corn-Plished Burger (comes with corn salsa)", false);
+INSERT INTO burgers (burgername, devoured) VALUES ("Foot Feta-ish Burger", false);
 INSERT INTO burgers (burgername, devoured) VALUES ("These Collards Don't Run Burger", false);
 INSERT INTO burgers (burgername, devoured) VALUES ("Last of the Mo-Jicama Burger (comes with jicama)", false);
 INSERT INTO burgers (burgername, devoured) VALUES ("Olive and Let Die Burger", false);
